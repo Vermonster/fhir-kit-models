65d096ffcd6a6c9347995b2a7125732edd379b92
Paul Wilson <paul@vermonster.com>
2018-06-08 10:52:55 -0400

    Make package consumable (#2)
    * Move resources to dist folder
    
    * Regenerate resources and gitingore dist
    
    * rename lib src
    
    * create index.js file
    
    * Move example to folder
    
    * avoid big diff in pr
    
    * Respond to comments

---
+++ src/generate.js
@@ -0,0 +1,134 @@
+const fs = require('fs');
+const { normalize } = require('path');
+const rimraf = require('rimraf');
+
+const Handlebars = require('handlebars'); // eslint-disable-line import/no-extraneous-dependencies
+
+const baseTemplatepath = normalize(`${__dirname}/templates`);
+
+const loadResourceTemplates = () => {
+  const baseTemplateFilename = 'element-template.hbs';
+  const templateFileNames = fs.readdirSync(baseTemplatepath);
+  templateFileNames.forEach(filename => {
+    if (filename === baseTemplateFilename || filename === 'index.hbs') {
+      return;
+    }
+    const template = fs.readFileSync(normalize(`${baseTemplatepath}/${filename}`), 'utf8');
+    Handlebars.registerPartial(filename.slice(0, filename.length - 4), template);
+  });
+  const baseTemplate = fs.readFileSync(normalize(`${baseTemplatepath}/${baseTemplateFilename}`), 'utf8');
+  return Handlebars.compile(baseTemplate);
+};
+
+const propertyParams = ([name, properties]) => {
+  const array = properties.type === 'array';
+  const objectProperties = array ? properties.items : properties;
+  const type = objectProperties.$ref ? objectProperties.$ref.slice(objectProperties.$ref.lastIndexOf('/') + 1) : objectProperties.type;
+
+  const primitive = !objectProperties.$ref;
+  const anyResource = type === 'ResourceList';
+  return {
+    name,
+    type: type.replace('_', ''),
+    array,
+    primitive,
+    anyResource,
+  };
+};
+
+const renderClass = (params, outputPath, template) => {
+  const output = `${template(params)}\n`;
+  fs.writeFileSync(normalize(`${outputPath}/${params.className}.js`), output);
+};
+
+const classParams = definition => {
+  const rawParams = definition.allOf[definition.allOf.length - 1];
+  return Object.entries(rawParams.properties).map(propertyParams);
+};
+
+const generateClass = (schema, outputPath, template) => {
+  for (const [className, definition] of Object.entries(schema.definitions)) {
+    const camelCaseClassName = className.replace('_', '');
+    console.log(`Generating: ${camelCaseClassName}.js`); // eslint-disable-line no-console
+    let superClass;
+    const definitionReference = definition.allOf[0].$ref;
+    if (definitionReference) {
+      superClass = definitionReference.slice(definitionReference.lastIndexOf('/') + 1);
+    }
+    const params = classParams(definition);
+
+    const usesArrayProxy = params.some(properties => properties.array && !properties.primitive);
+    const usesIsPresent = !superClass || params.some(param => !param.primitive);
+    const usesLoadResource = params.some(param => param.anyResource && !param.array);
+    const isResource = params.some(param => param.name === 'resourceType');
+
+    renderClass(
+      {
+        className: camelCaseClassName,
+        superClass: superClass && superClass.replace('_', ''),
+        usesArrayProxy,
+        usesIsPresent,
+        usesLoadResource,
+        isResource,
+        params,
+      },
+      outputPath,
+      template,
+    );
+  }
+};
+
+const generateEntryPont = destinationPath => {
+  const resourceFileNames = fs.readdirSync(normalize(`${destinationPath}`)).map(fileName => fileName.slice(0, fileName.indexOf('.')));
+  const entryPointTemplate = Handlebars.compile(fs.readFileSync(normalize(`${baseTemplatepath}/index.hbs`), 'utf8'));
+  const output = entryPointTemplate({ resourceFileNames });
+
+  console.log('Generating: index.js'); // eslint-disable-line no-console
+  fs.writeFileSync(normalize(`${destinationPath}/index.js`), output);
+};
+
+const copyFilesSync = (staticFiles, destinationPath) => {
+  const sourcePath = normalize(`${__dirname}/../src`);
+
+  staticFiles.forEach(file => {
+    fs.copyFileSync(`${sourcePath}/${file}`, `${destinationPath}/${file}`, err => {
+      if (err) throw err;
+      console.log(`Generating: ${file}`); // eslint-disable-line no-console
+    });
+  });
+};
+
+const generateClasses = (schemaFileNames, outputPath, template) => {
+  for (const schemaFileName of schemaFileNames) {
+    if (schemaFileName !== 'ResourceList.schema.json' && schemaFileName !== 'fhir.schema.json') {
+      const schema = JSON.parse(fs.readFileSync(normalize(`${__dirname}/../resource-schemas/${schemaFileName}`), 'utf8'));
+      generateClass(schema, outputPath, template);
+    }
+  }
+};
+
+const initializeDirectory = outputPath => {
+  rimraf.sync(outputPath);
+  fs.mkdirSync(outputPath);
+};
+
+const createClasses = () => {
+  // TODO Consider moving resourceTemplate to generateEntryPont
+  const resourceTemplate = loadResourceTemplates();
+  const destinationPath = normalize(`${__dirname}/../dist`);
+  const schemaFileNames = fs.readdirSync(normalize(`${__dirname}/../resource-schemas`));
+
+  initializeDirectory(destinationPath);
+  generateClasses(schemaFileNames, destinationPath, resourceTemplate);
+  generateEntryPont(destinationPath);
+  copyFilesSync(['helpers.js', 'ArrayProxy.js'], destinationPath);
+};
+
+module.exports = {
+  loadResourceTemplates,
+  generateClass,
+  initializeDirectory,
+  generateEntryPont,
+  copyFilesSync,
+  createClasses,
+};
